55876d3d6991dbcfdae43022f029018656a96535
413k5y <30969@s.pm.szczecin.pl>
2025-01-29 13:19:23 +0100

    .

--- ManeruMain.py
+++ ManeruMain.py
@@ -1,5 +1,18 @@
+import nextcord
+from nextcord.ext import commands
+from nextcord import Interaction, SlashOption
+from nextcord import permissions
+
+
 def main():
-    pass
+    client = commands.Bot()
+
+    @client.slash_command(description="Ping - pong!")
+    async def ping(interaction: nextcord.Interaction):
+        await interaction.send("Pong!", ephemeral=True)
+
+    client.run('token')
+
 
 if __name__ == '__main__':
     main()
